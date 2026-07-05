d5a3ae88c5116ba9f594b841186b159db8beaa24
ShanLaw876 <133469296+ShanLaw876@users.noreply.github.com>
2026-07-05 10:59:52 -0400

    form js added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-//things
+// Shannon Lawrence
+// Prekeh Landing Page
+// Filename: script.js
+// Create Date: July 4, 2026
+
+
+const form = document.getElementById("waitlist-form");
+const formMsg = document.getElementById("form-message");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const firstName = document.getElementById("firstname").value.trim();
+    const lastName = document.getElementById("lastname").value.trim();
+    const email = document.getElementById("email").value.trim();
+
+    console.log(firstName, lastName, email);
+    formMsg.textContent = `Thanks ${firstName}! You're on the Prekeh waitlist.`;
+   
+    form.reset();
+});
+
+
